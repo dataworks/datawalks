e7fad83a8211d7976715f685ecab5956ff799afa
Hayato Gibbons <hayato@ip-172-31-61-168.ec2.internal>
2015-06-10 09:20:28 -0400

    connects to database and heatmap

--- ui/src/resources/public/js/controllers/display.js
+++ ui/src/resources/public/js/controllers/display.js
@@ -1,3 +1,68 @@
 controllers.controller('Display', ['$scope', 'Watch', function($scope, Watch) {
-	$scope.records = Watch.query({id: 1, startDate: '2015-06-08 00:00:00', stopDate: '2015-06-08 23:59:59'});
+	$scope.map;
+	var pointarray;
+	var heatmap;
+	var watchData = [];
+	
+	$scope.loadMap = function(results) {
+	  var mapOptions = {
+	    zoom: 13,
+	    center: new google.maps.LatLng($scope.records.rows[0].latitude, $scope.records.rows[0].longitude)
+	  };
+	  $scope.map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+	  for(var i = 0; i < $scope.records.rows.length; i++)
+	  {
+	  	watchData.push(new google.maps.LatLng($scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
+	  }
+	  
+	  var pointArray = new google.maps.MVCArray(watchData);
+	  heatmap = new google.maps.visualization.HeatmapLayer({
+		    data: pointArray
+		  });
+
+	  heatmap.setMap($scope.map);
+	}
+	
+	$scope.toggleHeatmap = function() {
+		  heatmap.setMap(heatmap.getMap() ? null : $scope.map);
+		}
+
+	$scope.changeGradient = function() {
+		  var gradient = [
+		    'rgba(0, 255, 255, 0)',
+		    'rgba(0, 255, 255, 1)',
+		    'rgba(0, 191, 255, 1)',
+		    'rgba(0, 127, 255, 1)',
+		    'rgba(0, 63, 255, 1)',
+		    'rgba(0, 0, 255, 1)',
+		    'rgba(0, 0, 223, 1)',
+		    'rgba(0, 0, 191, 1)',
+		    'rgba(0, 0, 159, 1)',
+		    'rgba(0, 0, 127, 1)',
+		    'rgba(63, 0, 91, 1)',
+		    'rgba(127, 0, 63, 1)',
+		    'rgba(191, 0, 31, 1)',
+		    'rgba(255, 0, 0, 1)'
+		  ]
+		  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+		}
+	$scope.invert = function()
+	{
+		console.log(heatmap.getData().WeightedLocation.weight);
+		
+		//heatmap.set('radius', heatmap.get('radius')? null:20)
+	}
+	$scope.changeRadius = function() {
+		  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+		}
+
+	$scope.changeOpacity = function() {
+		  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+		}
+
+	
+	$scope.records = Watch.query({id: 1, startDate: '2015-06-08 00:00:00', stopDate: '2015-06-08 23:59:59'}, 
+			$scope.loadMap);
+
 }]);

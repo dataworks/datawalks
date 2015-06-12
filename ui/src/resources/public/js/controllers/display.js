3c8563e967f8908e82558ace9bc874800b9a817e
hayato.gibbons <hayato.gibbons@dataworks-inc.com>
2015-06-12 15:38:19 -0400

    working, but limited user selection

--- ui/src/resources/public/js/controllers/display.js
+++ ui/src/resources/public/js/controllers/display.js
@@ -4,27 +4,16 @@ controllers.controller('Display', ['$scope', 'Watch', function($scope, Watch) {
 	$scope.endlist = [];
     $scope.text = 'start date';
     $scope.endtext = 'end date'
-    $scope.ids  = [
-      {
-		id: "1"
-      }, 
-      {
-		id: "2"
-      }, 
-      {
-		id: "3"
-      },
-      {
-  		id: "4"
-      },
-      {
-  		id: "5"
-      }];
-    $scope.curr = 3897655761;
+    $scope.curr =[];
+    $scope.ids  = {
+		id1: 'NO',	
+		id2: 'NO'			
+    };
     
 	var pointarray;
 	var heatmap;
-	var watchData = [];
+	var heatmap2;
+	//var watchData = [];
 	
 	$scope.loadMap = function(results) {
 	  var mapOptions = {
@@ -35,44 +24,102 @@ controllers.controller('Display', ['$scope', 'Watch', function($scope, Watch) {
 	      mapOptions);
 	  
 	}
+	
+	function hM1init(text1, text2)
+	{
+		var watchData = [];
+        var st = text1.split("-");
+        var en = text2.split("-");
+		for(var i = 0; i < $scope.records.rows.length; i++)
+		{
+      	  var dt = new Date($scope.records.rows[i].dtime);
+      	  
+      	  if($scope.records.rows[i].deviceid === $scope.curr[0] 
+      			  && (dt.getDate() >= Number(st[2]) && dt.getDate() <= Number(en[2])))
+      	  {
+      		  //console.log(dt.getDate());
+      		  watchData.push(new google.maps.LatLng(
+      				  $scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
+      	  }
+		}
+		
+		 var pointArray = new google.maps.MVCArray(watchData);
+		  heatmap = new google.maps.visualization.HeatmapLayer({
+			    data: pointArray
+			  });
+
+		  heatmap.setMap($scope.map);
+	}
+	function hM2init(text1, text2)
+	{
+		var watchData = [];
+        var st = text1.split("-");
+        var en = text2.split("-");
+		for(var i = 0; i < $scope.records.rows.length; i++)
+		{
+      	  var dt = new Date($scope.records.rows[i].dtime);
+      	  
+      	  if($scope.records.rows[i].deviceid === $scope.curr[1] 
+      			  && (dt.getDate() >= Number(st[2]) && dt.getDate() <= Number(en[2])))
+      	  {
+      		  //console.log(dt.getDate());
+      		  watchData.push(new google.maps.LatLng(
+      				  $scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
+      	  }
+		}
+		
+		 var pointArray = new google.maps.MVCArray(watchData);
+		  heatmap2 = new google.maps.visualization.HeatmapLayer({
+			    data: pointArray
+			  });
+		  
+		  var gradient = [
+		     'rgba(0, 255, 255, 0)',
+		     'rgba(0, 255, 255, 1)',
+		     'rgba(0, 191, 255, 1)',
+		     'rgba(0, 127, 255, 1)',
+		     'rgba(0, 63, 255, 1)',
+		     'rgba(0, 0, 255, 1)',
+		     'rgba(0, 0, 223, 1)',
+		     'rgba(0, 0, 191, 1)',
+		     'rgba(0, 0, 159, 1)',
+		     'rgba(0, 0, 127, 1)',
+		     'rgba(63, 0, 91, 1)',
+		     'rgba(127, 0, 63, 1)',
+		     'rgba(191, 0, 31, 1)',
+		     'rgba(255, 0, 0, 1)'
+		  ]
+		  heatmap2.set('gradient', heatmap2.get('gradient') ? null : gradient);
+
+		  heatmap2.setMap($scope.map);
+	}
+	
 	$scope.submit = function() 
 	{
-		$scope.list.pop();
-		$scope.endlist.pop();
-		watchData = [];
 		if(heatmap != null)
 		{
 			heatmap.setMap(null);
 		}
-		
-		if ($scope.text) 
+		else if(heatmap2 != null)
 		{
-	          $scope.list.push(this.text);
-	          $scope.endlist.push(this.endtext);
-	          $scope.text = '';
-	          $scope.endtext = '';
-	          var st = $scope.list[0].split("-");
-	          var en = $scope.endlist[0].split("-");
-	          var dt = new Date($scope.records.rows[0].dtime);
-        	  console.log(dt.getDate());
-	          for(var i = 0; i < $scope.records.rows.length; i++)
-			  {
-	        	  var dt = new Date($scope.records.rows[i].dtime);
-	        	  if($scope.records.rows[i].deviceid === $scope.curr 
-	        			  && (dt.getDate() >= st[2] && dt.getDate() <= en[2]))
-	        	  {
-	        		  watchData.push(new google.maps.LatLng(
-	        				  $scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
-	        	  }
-			  }
-			  
-			  var pointArray = new google.maps.MVCArray(watchData);
-			  heatmap = new google.maps.visualization.HeatmapLayer({
-				    data: pointArray
-				  });
-
-			  heatmap.setMap($scope.map);
-		}			
+			heatmap2.setMap(null);
+		}
+		if($scope.ids.id1 === 'YES')
+	    {
+	    	$scope.curr[0] = 3897655761;
+	    	$scope.list.pop();
+			$scope.endlist.pop();
+			$scope.list.push(this.text);
+			$scope.endlist.push(this.endtext);
+	        $scope.text = '';
+	        $scope.endtext = '';
+	    	hM1init($scope.list[0],$scope.endlist[0]);
+	    }
+	    if($scope.ids.id2 === 'YES')
+	    {
+	    	$scope.curr[1] = 3890500495;
+	    	hM2init($scope.list[0],$scope.endlist[0]);
+	    }
 	}
 	
 	$scope.toggleHeatmap = function() {

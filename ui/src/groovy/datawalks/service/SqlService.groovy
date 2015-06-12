270ccf5141fbc4030d0ffce9126f6cd7e65be6b0
lindsay.merriken <lindsay.merriken@dataworks-inc.com>
2015-06-12 16:05:49 -0400

    The "final"  product

--- ui/src/groovy/datawalks/service/SqlService.groovy
+++ ui/src/groovy/datawalks/service/SqlService.groovy
@@ -1,52 +1,35 @@
 package datawalks.service
 
 import groovy.sql.Sql
-
 import javax.sql.DataSource
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class SqlService {
-	//@Autowired JdbcTemplate jdbcTemplate
-	@Autowired DataSource dataSource
-	
-	def getGeoPoints(long watchId, Date startDate, Date stopDate) {
-		def rows = []
-		Sql sql = new Sql(dataSource)
-		sql.eachRow("""select deviceid device, latitude latitude, longitude longitude   
-						from workabledata""") 
-		{
-			rows << [device: it.device, latitude: it.latitude, longitude: it.longitude ]
-		}
-		//sql.eachRow("""select datetime datetime, longitudedegrees longitude, latitudedegrees latitude from dan_drive """) {
-			//rows << [datetime: it.datetime, latitude: it.latitude, longitude: it.longitude]
-		//}
-		return rows
-	}
-	//create jwc connection
-	//take in config
-	def getTotalDistance(long watchId, Date startDate, Date stopDate){
-		def rows = []
-		Sql sql = new Sql(dataSource)
-		sql.eachRow("""SELECT max(distancemeters) mdistance,to_char(dtime,'yyyy-mm-dd') dtime from workabledata
-						GROUP BY to_char(dtime,'yyyy-mm-dd');"""){
-			rows << [ dtime: it.dtime, mdistance: it.mdistance]
-		}
-		return rows
-	}
-	
-	def getGeoLat() {
-		def rows = []
-		Sql sql = new Sql(dataSource)
-		sql.eachRow("select latitudedegrees latitude from dan_drive where latitudedegrees > ?", [38.956] ) {
-			rows << [ latitude: it.latitude]
-		}
-		return rows
-	}
-	//returns the latitude degrees greater than 38.956
-	
-	
-	
+    @Autowired DataSource dataSource
+    
+    def getGeoPoints(long watchId, Date startDate, Date stopDate) {
+        def rows = []
+        Sql sql = new Sql(dataSource)
+        sql.eachRow("""select deviceid device, latitude latitude, longitude longitude   
+                        from workabledata""") 
+        {
+            rows << [device: it.device, latitude: it.latitude, longitude: it.longitude ]
+        }
+        return rows
+    }
+    
+    /* def - getTotalDistance()
+     * Returns the max distance for each day in the table workabledata
+     */
+    def getTotalDistance(long watchId, Date startDate, Date stopDate){
+        def rows = []
+        Sql sql = new Sql(dataSource)
+        sql.eachRow("""SELECT max(distancemeters) mdistance,to_char(dtime,'yyyy-mm-dd') dtime from workabledata
+                        GROUP BY to_char(dtime,'yyyy-mm-dd');"""){
+            rows << [ dtime: it.dtime, mdistance: it.mdistance]
+        }
+        return rows
+    }    
 }
--- ui/src/resources/public/js/controllers/display.js
+++ ui/src/resources/public/js/controllers/display.js
@@ -1,68 +1,116 @@
 controllers.controller('Display', ['$scope', 'Watch', function($scope, Watch) {
-	$scope.map;
-	var pointarray;
-	var heatmap;
-	var watchData = [];
-	
-	$scope.loadMap = function(results) {
-	  var mapOptions = {
-	    zoom: 13,
-	    center: new google.maps.LatLng($scope.records.rows[0].latitude, $scope.records.rows[0].longitude)
-	  };
-	  $scope.map = new google.maps.Map(document.getElementById('map-canvas'),
-	      mapOptions);
-	  for(var i = 0; i < $scope.records.rows.length; i++)
-	  {
-	  	watchData.push(new google.maps.LatLng($scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
-	  }
-	  
-	  var pointArray = new google.maps.MVCArray(watchData);
-	  heatmap = new google.maps.visualization.HeatmapLayer({
-		    data: pointArray
-		  });
+    $scope.map;
+    var pointarray;
+    var heatmap;
+    var watchData = [];
+    
+    $scope.loadMap = function() {
 
-	  heatmap.setMap($scope.map);
-	}
-	
-	$scope.toggleHeatmap = function() {
-		  heatmap.setMap(heatmap.getMap() ? null : $scope.map);
-		}
+      var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng($scope.records.rows[0].latitude, $scope.records.rows[0].longitude)
+      };
+      $scope.map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+      for(var i = 0; i < $scope.records.rows.length; i++)
+      {
+          watchData.push(new google.maps.LatLng($scope.records.rows[i].latitude, $scope.records.rows[i].longitude));
+      }
+      
+      var pointArray = new google.maps.MVCArray(watchData);
+      heatmap = new google.maps.visualization.HeatmapLayer({
+            data: pointArray
+          });
 
-	$scope.changeGradient = function() {
-		  var gradient = [
-		    'rgba(0, 255, 255, 0)',
-		    'rgba(0, 255, 255, 1)',
-		    'rgba(0, 191, 255, 1)',
-		    'rgba(0, 127, 255, 1)',
-		    'rgba(0, 63, 255, 1)',
-		    'rgba(0, 0, 255, 1)',
-		    'rgba(0, 0, 223, 1)',
-		    'rgba(0, 0, 191, 1)',
-		    'rgba(0, 0, 159, 1)',
-		    'rgba(0, 0, 127, 1)',
-		    'rgba(63, 0, 91, 1)',
-		    'rgba(127, 0, 63, 1)',
-		    'rgba(191, 0, 31, 1)',
-		    'rgba(255, 0, 0, 1)'
-		  ]
-		  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-		}
-	$scope.invert = function()
-	{
-		console.log(heatmap.getData().WeightedLocation.weight);
-		
-		//heatmap.set('radius', heatmap.get('radius')? null:20)
-	}
-	$scope.changeRadius = function() {
-		  heatmap.set('radius', heatmap.get('radius') ? null : 20);
-		}
+      heatmap.setMap($scope.map);
+      
+      
+    }
+    
+    $scope.toggleHeatmap = function() {
+          heatmap.setMap(heatmap.getMap() ? null : $scope.map);
+        }
 
-	$scope.changeOpacity = function() {
-		  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-		}
+    $scope.changeGradient = function() {
+          var gradient = [
+            'rgba(0, 255, 255, 0)',
+            'rgba(0, 255, 255, 1)',
+            'rgba(0, 191, 255, 1)',
+            'rgba(0, 127, 255, 1)',
+            'rgba(0, 63, 255, 1)',
+            'rgba(0, 0, 255, 1)',
+            'rgba(0, 0, 223, 1)',
+            'rgba(0, 0, 191, 1)',
+            'rgba(0, 0, 159, 1)',
+            'rgba(0, 0, 127, 1)',
+            'rgba(63, 0, 91, 1)',
+            'rgba(127, 0, 63, 1)',
+            'rgba(191, 0, 31, 1)',
+            'rgba(255, 0, 0, 1)'
+          ]
+          heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+        }
+    $scope.invert = function()
+    {
+        console.log(heatmap.getData().WeightedLocation.weight);
+        
+        //heatmap.set('radius', heatmap.get('radius')? null:20)
+    }
+    $scope.changeRadius = function() {
+          heatmap.set('radius', heatmap.get('radius') ? null : 20);
+        }
 
-	
-	$scope.records = Watch.query({id: 1, startDate: '2015-06-08 00:00:00', stopDate: '2015-06-08 23:59:59'}, 
-			$scope.loadMap);
+    $scope.changeOpacity = function() {
+          heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+        }
 
+     $scope.parseDate = function(input){
+        var parts = input.split('-');
+        return new Date(parts[0], parts[1]-1, parts[2]); 
+    }
+     
+  //Draw the CalendarChart
+  $scope.drawChart = function() {
+       
+      var dataTable = new google.visualization.DataTable();
+      dataTable.addColumn({ type: 'date', id: 'Date' });
+      dataTable.addColumn({ type: 'number', id: 'distance' });
+
+       for(var i = 0; i < $scope.records.aggs.length; i++)
+          {
+                dataTable.addRow( [ new Date(Date.parse($scope.records.aggs[i].dtime)), parseInt($scope.records.aggs[i].mdistance) ] );
+          }
+           
+       var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+    
+       var options = {
+             title: "Distance Traveled per Day",
+             height: 300,
+             width: 2000,
+             calendar: {
+                unusedMonthOutlineColor: {
+                      stroke: 'red',
+                      strokeOpacity: 0.8,
+                      strokeWidth: 2
+                    },
+                cellSize: 25
+             },
+               noDataPattern: {
+                  borderColor: 'black'
+                },
+            tooltip: {
+                isHtml: false
+                }
+           };
+    
+       chart.draw(dataTable, options); 
+  }
+  
+  $scope.recordsLoaded = function(results){
+       $scope.loadMap();
+       $scope.drawChart(); 
+  }
+  
+  $scope.records = Watch.query({id: 1, startDate: '2015-06-08 00:00:00', stopDate: '2015-06-08 23:59:59'}, 
+            $scope.recordsLoaded);
 }]);

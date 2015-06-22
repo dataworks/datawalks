0d1f2d1c1f17c91456d77d3786dc4896319ab067
hayato.gibbons <hayato.gibbons@dataworks-inc.com>
2015-06-22 12:51:24 -0400

    refactored calendar chart

--- ui/src/resources/public/js/controllers/chartDisplay.js
+++ ui/src/resources/public/js/controllers/chartDisplay.js
@@ -5,6 +5,19 @@
 controllers.controller('ChartDisplay', ['$scope', 'Watch', function($scope, Watch) {
 	//Draw the CalendarChart
 	$scope.deviceIds = [];
+	var dataTables = [];
+	var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+	var options = 
+ 	{
+ 		title: "Distance",
+ 	    height: 300,
+ 	    width: 2000,
+ 	    tooltip: 
+ 	    {
+		   	isHtml: false
+		},
+		   	calendar: { cellSize: 25 }
+ 	 };
 	
 	$scope.loadIds = function()
 	{
@@ -18,32 +31,35 @@ controllers.controller('ChartDisplay', ['$scope', 'Watch', function($scope, Watc
 		}
 	}
 	
-	$scope.drawChart = function() 
+	$scope.showIds = function(index)
 	{
-		$scope.loadIds();
-		var dataTable = new google.visualization.DataTable();
+		index--;
+		var dataTable = [];
+	    dataTable = new google.visualization.DataTable();
 	    dataTable.addColumn({ type: 'date', id: 'Date' });
 	    dataTable.addColumn({ type: 'number', id: 'distance' });
 	       
 	 	for(var i = 0; i < $scope.records.aggs.length; i++)
 	 	{
-	 		dataTable.addRow( [ new Date(Date.parse($scope.records.aggs[i].dtime)), parseInt($scope.records.aggs[i].mdistance) ] );
+	 		if($scope.deviceIds[index].id == $scope.records.aggs[i].did)
+	 		{
+	 			dataTable.addRow( [ new Date(Date.parse($scope.records.aggs[i].dtime)), 
+	 			                            parseInt($scope.records.aggs[i].mdistance)] );
+	 		}
 	 	}
-	 	       
-	 	var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
-	 	
-	 	var options = 
-	 	{
-	 		title: "Distance",
-	 	    height: 300,
-	 	    width: 2000,
-	 	    tooltip: 
-	 	    {
-			   	isHtml: false
-			},
-			   	calendar: { cellSize: 25 }
-	 	 };	 	
-	 	 chart.draw(dataTable, options); 
+	 	chart.draw(dataTable, options);
+	 	//$scope.updateChart(index);
+	}
+	
+	$scope.drawChart = function() 
+	{
+		$scope.loadIds();
+		
+		var dataTable = new google.visualization.DataTable();
+		
+		dataTable.addColumn({ type: 'date', id: 'Date' });
+	    dataTable.addColumn({ type: 'number', id: 'distance' });
+		chart.draw(dataTable, options);
 	}
 	$scope.recordsLoaded = function(results)
 	{

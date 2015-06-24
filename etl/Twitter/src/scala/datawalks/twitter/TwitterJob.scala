6bbe13f12f8bfd349644ba02a22404e7bd8ef114
Robert Graham <robert.graham@dataworks-inc.com>
2015-06-24 16:15:43 -0400

    Twitter Spark Job.

--- etl/Twitter/src/scala/datawalks/twitter/TwitterJob.scala
+++ etl/Twitter/src/scala/datawalks/twitter/TwitterJob.scala
@@ -4,24 +4,41 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.elasticsearch.spark._
 import org.apache.spark.sql.SQLContext
+import java.text.SimpleDateFormat
+import java.util.Properties
+import java.io.FileInputStream
 
 object TwitterJob {
-  private var DB_QUERY = "(SELECT * FROM tname) tname"
-  
+  private var DB_QUERY = """(SELECT * FROM(
+    SELECT round(cast(latitude AS NUMERIC), 4) latitude, round(cast(longitude AS NUMERIC), 4) longitude, TO_CHAR(dtime, 'YYYY-MM-DD') dtime, COUNT(*) frequency FROM workabledata  
+    GROUP BY round(cast(latitude AS NUMERIC), 4), round(cast(longitude AS NUMERIC), 4), TO_CHAR(dtime, 'YYYY-MM-DD')
+    ORDER BY COUNT(*) DESC) AS toppoints
+    WHERE frequency >= 20) dbquery"""
+
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Twitter")
+    if (args.length < 1) {
+      println("Usage: TwitterJob <PropertyFile>")
+      System.exit(1)
+    }
+    val conf = new SparkConf().setAppName("Twitter").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
+    val properties = new Properties()
+    properties.load(new FileInputStream(args(0)))
+
     val dataFrame = sqlContext.load("jdbc", Map(
-      "url" ->"jdbc:postgresql://host/database?user=&password=",
+      "url" -> properties.getProperty("db.url"),
       "dbtable" -> DB_QUERY))
 
     // For each lat/lon match run TwitterQuery
-    val query = new TwitterQuery()
-    val tweets = query.execute()
-
-    sc.parallelize(tweets).saveToEs("twitter/tweets")
+    dataFrame.collect().foreach { r =>
+      val date = new SimpleDateFormat("yyyy-MM-dd").parse(r.getString(2))
+      val query = new TwitterQuery()
+      query.setAuthentication(properties)
+      val tweets = query.execute(r.getDecimal(0).doubleValue(), r.getDecimal(1).doubleValue(), date)
+      sc.parallelize(tweets).saveToEs("twitter/tweets")
+    }
 
     sc.stop()
   }
--- etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
+++ etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
@@ -1,20 +1,37 @@
 package datawalks.twitter
 
-import java.util.Collection
-import twitter4j.TwitterFactory
-import java.util.ArrayList
-import twitter4j.Query
-import scala.collection.JavaConversions._
+import scala.collection.JavaConversions.asScalaBuffer
 import scala.collection.mutable.ListBuffer
+import twitter4j.GeoLocation
+import twitter4j.Query
+import twitter4j.TwitterFactory
+import twitter4j.conf.ConfigurationBuilder
+import java.util.Date
+import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.Properties
+import twitter4j.conf.Configuration
 
 class TwitterQuery {
-  def setAuthentication() {
+  private var config : Configuration = null
+
+  def setAuthentication(properties: Properties){
+    var cb = new ConfigurationBuilder()
+    cb.setOAuthConsumerKey(properties.getProperty("twitter.consumerKey"))
+    cb.setOAuthConsumerSecret(properties.getProperty("twitter.consumerSecret"))
+    cb.setOAuthAccessToken(properties.getProperty("twitter.accessToken"))
+    cb.setOAuthAccessTokenSecret(properties.getProperty("twitter.accessTokenSecret"))
+    
+    config = cb.build()
   }
 
-  def execute(): Seq[Map[String, Any]] = {
+  def execute(latitude: Double, longitude: Double, date: Date): Seq[Map[String, Any]] = {
     val tweets = new ListBuffer[Map[String, Any]]()
-    val twitter = TwitterFactory.getSingleton()
-    val query = new Query("test query")
+    val twitter = new TwitterFactory(config).getInstance();
+
+    val locus = new GeoLocation(latitude, longitude)
+    var query = new Query(getDateQuery(date))
+    query = query.geoCode(locus, 1, "mi")
     val result = twitter.search(query)
     result.getTweets().foreach { status =>
       val hasGeoLocation = status.getGeoLocation() != null
@@ -26,5 +43,20 @@ class TwitterQuery {
     }
 
     return tweets
+  }
+
+  private def getDateQuery(date: Date): String = {
+    var query = "since:"
+    query += new SimpleDateFormat("yyyy-MM-dd").format(date)
+    query += " until:"
+    query += new SimpleDateFormat("yyyy-MM-dd").format(addDays(date, 1))
+    return query
+  }
+
+  private def addDays(date: Date, days: Integer) : Date = {
+    val cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.add(Calendar.DATE, days);
+    return cal.getTime();
   }
 }

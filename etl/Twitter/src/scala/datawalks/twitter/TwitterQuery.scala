e8ab3af43671dc9cd9296dd4e7cb5969430606e9
Robert Graham <robert.graham@dataworks-inc.com>
2015-06-26 16:45:35 -0400

    Added ability to upload results to Elasticsearch Server. Future functionality (duplicate prevention) currently commented out.

--- etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
+++ etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
@@ -11,17 +11,19 @@ import java.util.Calendar
 import java.text.SimpleDateFormat
 import java.util.Properties
 import twitter4j.conf.Configuration
+import twitter4j.QueryResult
+import twitter4j.Twitter
 
 class TwitterQuery {
-  private var config : Configuration = null
+  private var config: Configuration = null
 
-  def setAuthentication(properties: Properties){
+  def setAuthentication(properties: Properties) {
     var cb = new ConfigurationBuilder()
     cb.setOAuthConsumerKey(properties.getProperty("twitter.consumerKey"))
     cb.setOAuthConsumerSecret(properties.getProperty("twitter.consumerSecret"))
     cb.setOAuthAccessToken(properties.getProperty("twitter.accessToken"))
     cb.setOAuthAccessTokenSecret(properties.getProperty("twitter.accessTokenSecret"))
-    
+
     config = cb.build()
   }
 
@@ -30,17 +32,21 @@ class TwitterQuery {
     val twitter = new TwitterFactory(config).getInstance();
 
     val locus = new GeoLocation(latitude, longitude)
-    var query = new Query(getDateQuery(date))
-    query = query.geoCode(locus, 1, "mi")
-    val result = twitter.search(query)
-    result.getTweets().foreach { status =>
-      val hasGeoLocation = status.getGeoLocation() != null
-      val record = Map("user" -> status.getUser().getName(), "text" -> status.getText(), "date" -> status.getCreatedAt(),
-        "latitude" -> (if (hasGeoLocation) status.getGeoLocation().getLatitude() else null),
-        "longitude" -> (if (hasGeoLocation) status.getGeoLocation().getLongitude() else null))
+    var query = new Query(getDateQuery(date)).count(100)
+    query = query.geoCode(locus, 50, "mi")
+    var result = twitter.search(query)
+    do {
+      result.getTweets().foreach { status =>
+        val hasGeoLocation = status.getGeoLocation() != null
+        val record = Map("user" -> status.getUser().getName(), "text" -> status.getText(), "date" -> status.getCreatedAt(),
+          "latitude" -> (if (hasGeoLocation) status.getGeoLocation().getLatitude() else null),
+          "longitude" -> (if (hasGeoLocation) status.getGeoLocation().getLongitude() else null),
+          "image" -> status.getUser().getOriginalProfileImageURL)
 
-      tweets += record
-    }
+        tweets += record
+      }
+      result = getNextResult(twitter, result)
+     } while (result != null)
 
     return tweets
   }
@@ -53,10 +59,17 @@ class TwitterQuery {
     return query
   }
 
-  private def addDays(date: Date, days: Integer) : Date = {
+  private def addDays(date: Date, days: Integer): Date = {
     val cal = Calendar.getInstance();
     cal.setTime(date);
     cal.add(Calendar.DATE, days);
     return cal.getTime();
+  }
+
+  private def getNextResult(twitter : Twitter, result : QueryResult) : QueryResult = {
+    if (result.hasNext()) {
+      return twitter.search(result.nextQuery())
+    }
+    return null
   }
 }

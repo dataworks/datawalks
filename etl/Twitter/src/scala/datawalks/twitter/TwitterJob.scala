f278849b4e9c4c34920db69e11f242d2989ba47f
Robert Graham <robert.graham@dataworks-inc.com>
2015-06-30 14:18:04 -0400

    Added comments.

--- etl/Twitter/src/scala/datawalks/twitter/TwitterJob.scala
+++ etl/Twitter/src/scala/datawalks/twitter/TwitterJob.scala
@@ -13,7 +13,16 @@ import java.io.File
 import java.util.Calendar
 import scala.io.Source
 
+/*
+ * Job to build Spark job that:
+ *  -creates a twitter query based on database information from PostgreSQL
+ *  -grab tweets from twitter's REST api
+ *  -save tweet information to elasticsearch index
+ *  -keep record of what tweets are already indexed
+ */
 object TwitterJob {
+
+  //SQL statement to grab information from Postgres
   private var DB_QUERY = """(SELECT * FROM(
     SELECT round(cast(latitude AS NUMERIC), 4) latitude, round(cast(longitude AS NUMERIC), 4) longitude, TO_CHAR(dtime, 'YYYY-MM-DD') dtime, COUNT(*) frequency, dateinserted dateinserted FROM workabledata  
     WHERE dateinserted > 'DATE_INSERTED'
@@ -22,33 +31,47 @@ object TwitterJob {
     WHERE frequency >= 5) dbquery"""
 
   def main(args: Array[String]) {
+    
+    //check to make sure property file is being passed in from command line
     if (args.length < 1) {
       println("Usage: TwitterJob <PropertyFile>")
       System.exit(1)
     }
 
+    //load properties file
     val properties = new Properties()
     properties.load(new FileInputStream(args(0)))
 
+    //read the most recent date of twitter acquisition
     val recentDate = Source.fromFile("recentDate.txt").mkString
 
+    /*
+     * Build Spark Config and Context, and SQL Context
+     * set where Elasticsearch server information is located in properties file
+     */
     val conf = new SparkConf().setAppName("Twitter").setMaster("local[2]")
       .set("es.nodes", properties.getProperty("es.nodes"))
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
+    /*
+     * Build dataframe from Postgres using URL from properties file
+     * Run query specified above
+     */
     val dataFrame = sqlContext.load("jdbc", Map(
       "url" -> properties.getProperty("db.url"),
       "dbtable" -> DB_QUERY.replaceAll("DATE_INSERTED", recentDate)))
 
+    //write current date to text file
     val pw = new PrintWriter(new File("recentDate.txt"))
     val format = new SimpleDateFormat("yyyy-MM-dd")
     pw.write(format.format(Calendar.getInstance().getTime()))
     pw.close()
 
+    //grab Elasticsearch index to populate
     val esIndex = properties.getProperty("es.resource")
 
-    // For each lat/lon match run TwitterQuery
+    // For each lat/lon match run TwitterQuery, and save to Elasticsearch
     dataFrame.collect().foreach { r =>
       val date = new SimpleDateFormat("yyyy-MM-dd").parse(r.getString(2))
       val query = new TwitterQuery()
@@ -56,6 +79,7 @@ object TwitterJob {
       val tweets = query.execute(r.getDecimal(0).doubleValue(), r.getDecimal(1).doubleValue(), date)
       sc.parallelize(tweets).saveToEs(esIndex)
     }
+    //stop spark context
     sc.stop()
   }
 }
--- etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
+++ etl/Twitter/src/scala/datawalks/twitter/TwitterQuery.scala
@@ -14,9 +14,18 @@ import twitter4j.conf.Configuration
 import twitter4j.QueryResult
 import twitter4j.Twitter
 
+/*
+ * Class to build a query against Twitter.
+ */
 class TwitterQuery {
+
+  // Initialize a null Configuration to be filled later
   private var config: Configuration = null
 
+  /*
+   * Method to set the OAuth authentication properties required to pull tweets.
+   * Properties pulled from run.properties file.
+   */
   def setAuthentication(properties: Properties) {
     var cb = new ConfigurationBuilder()
     cb.setOAuthConsumerKey(properties.getProperty("twitter.consumerKey"))
@@ -27,14 +36,37 @@ class TwitterQuery {
     config = cb.build()
   }
 
+ /*
+  * Method to build the query and search Twitter.
+  */
   def execute(latitude: Double, longitude: Double, date: Date): Seq[Map[String, Any]] = {
+
+    //initialize empty ListBuffer and TwitterFactory
     val tweets = new ListBuffer[Map[String, Any]]()
     val twitter = new TwitterFactory(config).getInstance();
 
+    //using lat and lon parameters, build the GeoLocation to be searched around
     val locus = new GeoLocation(latitude, longitude)
+
+    //initialize query using date passed, 50 tweets per page 
     var query = new Query(getDateQuery(date)).count(50)
+
+    //add the geolocation, radius, and unit of measurement to the query
     query = query.geoCode(locus, 2, "mi")
+
+    //execute search
     var result = twitter.search(query)
+
+    /*
+     * Iteratively grab tweets with properties:
+     * 
+     *  user - Name of the user             handle - user's twitter handle
+     *  userID - user's numerical ID        text - text of grabbed tweet
+     *  date - date tweet was created       latitude - latitude of tweet
+     *  longitude - longitude of tweet      image - get user's profile picture
+     *  
+     * Concatenate each tweet record to Map 
+     */
     do {
       result.getTweets().foreach { status =>
         val hasGeoLocation = status.getGeoLocation() != null
@@ -55,6 +87,9 @@ class TwitterQuery {
     return tweets
   }
 
+  /*
+   * Build date query - looking from date specified, until immediately subsequent date
+   */
   private def getDateQuery(date: Date): String = {
     var query = "since:"
     query += new SimpleDateFormat("yyyy-MM-dd").format(date)
@@ -63,6 +98,9 @@ class TwitterQuery {
     return query
   }
 
+  /*
+   * Method for adding Days to SimpleDateFormat Dates.
+   */
   private def addDays(date: Date, days: Integer): Date = {
     val cal = Calendar.getInstance();
     cal.setTime(date);
@@ -70,6 +108,9 @@ class TwitterQuery {
     return cal.getTime();
   }
 
+  /*
+   * Method to check if more results are present, and if so, return them.
+   */
   private def getNextResult(twitter: Twitter, result: QueryResult): QueryResult = {
     if (result.hasNext()) {
       return twitter.search(result.nextQuery())
